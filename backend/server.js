36e7a9a8d6cf00bd4543d0d570beb2177a937d81
Aravindswamymajjuri <aravindswamymajjuri143@gmail.com>
2025-02-12 22:12:20 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,10 @@ mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
-    console.log('MongoDB connected to PN database...');
+    console.log('MongoDB connected...');
+}).catch(err => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
 });
 
 const connectedUsers = new Map();
